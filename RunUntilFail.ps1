4f48982a8aad3d97bb4ab34ec413d8af973cf84b
Gennadii Saltyshchak <GennadyGS@gmail.com>
2021-12-14 10:04:58 +0200

    Fix error and improve logging

--- RunUntilFail.ps1
+++ RunUntilFail.ps1
@@ -8,18 +8,18 @@ param (
 $attempt = 0
 Do {
     $attempt++ 
-    Write-Output "Attempt #$attempt to run command '$command'"
+    Write-Output "Attempt $attempt of $maxAttempts to run command '$command'"
     Invoke-Expression $command
     if ($LastExitCode -gt $successExitCode) {
-        Write-Warning "Attempt #$attempt of command '$command' is finished with error exit code $LastExitCode"
+        Write-Warning "Attempt $attempt of $maxAttempts of command '$command' is finished with error exit code $LastExitCode"
         Break
     }
-    "Command is finished with success exit code $LastExitCode"
+    "Attempt $attempt of $maxAttempts is finished with success exit code $LastExitCode"
     if ($attempt -ge $maxAttempts) {
         Write-Output "All $attempt attempts of command '$command' have passed"
         Break
     } else {
-        Write-Warning "Retry to run command after $timeoutSeconds s"
+        Write-Output "Retrying to run command after $timeoutSeconds s"
         Start-Sleep -s $timeoutSeconds
     }
-} While(True)
+} While($True)
