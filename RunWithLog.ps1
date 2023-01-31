a394d8aab5a70dd5e69f5109561c9ff01eff2208
Gennadii Saltyshchak <GennadyGS@gmail.com>
2023-01-31 14:38:06 +0200

    Write logs to separate directory

--- RunWithLog.ps1
+++ RunWithLog.ps1
@@ -4,5 +4,7 @@ param
     [Parameter(Mandatory=$false, Position=1, ValueFromRemainingArguments=$true)] $commandArgs
 )
 filter timestamp { "$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss.fff'): $_" }
-$logFileName = "$($command)_$(Get-Date -Format yyyyMMdd_HHmmss).log"
+$logDirectoryPath = Join-Path $PSScriptRoot Log
+New-Item -ItemType Directory -Force -Path $logDirectoryPath | Out-Null
+$logFileName = Join-Path $logDirectoryPath "$($command)_$(Get-Date -Format yyyyMMdd_HHmmss).log"
 & $command $commandArgs | timestamp | Tee-Object $logFileName
