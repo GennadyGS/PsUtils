ce06b3ca83147d69074d6e95a1cab4c660b614e5
Gennadii Saltyshchak <GennadyGS@gmail.com>
2023-06-19 16:11:52 +0300

    Set timeout to AddPath operation

--- AddPath.ps1
+++ AddPath.ps1
@@ -7,5 +7,17 @@ $varName = "PATH"
 $fullPath = [System.IO.Path]::GetFullPath($path)
 $oldValue = [Environment]::GetEnvironmentVariable($varName, $scope)
 $newValue = "$oldValue;$fullPath"
-[Environment]::SetEnvironmentVariable($varName, $newValue, $scope)
-"Path '$fullPath' is added to environment variable $varName (scope $scope)"
+"Adding path '$fullPath' to environment variable $varName (scope $scope)..."
+$job = Start-Job `
+    -ScriptBlock {
+        param ($varName, $newValue, $scope)
+        [Environment]::SetEnvironmentVariable($varName, $newValue, $scope)
+    } -ArgumentList $varName, $newValue, $scope
+$job | Wait-Job -Timeout 5
+If ($job.State -eq [Management.Automation.JobState]::Completed) {
+    "Path is successfully added to environment variable"
+}
+Else {
+    Write-Error "Job is not completed successfully and has state $($job.State)"
+    Remove-Job $job -Force
+}
