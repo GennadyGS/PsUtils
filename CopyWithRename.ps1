f8dab4624aa2db91329797c3a5399b4f56cbe138
Gennadii Saltyshchak <GennadyGS@gmail.com>
2024-02-15 14:30:20 +0200

    Add script CopyWithRename.ps1

---
+++ CopyWithRename.ps1
@@ -0,0 +1,30 @@
+param(
+    [Parameter(mandatory=$true)] $path,
+    [Parameter(mandatory=$true)] $destination
+)
+
+Function FindUniqueFileName($fileName) {
+    $result = $fileName
+    $i = 0
+    While (Test-Path $result -PathType Leaf) {
+        $i += 1
+        $parentPath = Split-Path $fileName -Parent
+        $fileNameBase = Split-Path $fileName -LeafBase
+        $extension = Split-Path $fileName -Extension
+        $establishedFileName = $fileNameBase + " ($i)" + $extension
+        $result = Join-Path $parentPath $establishedFileName
+    }
+    return $result
+}
+
+If (!(Test-Path $path -PathType Leaf)) {
+    throw "Source file $path does not exist"
+}
+
+$destinationFileName = (Test-Path $destination -PathType Container) `
+    ? (Join-Path $destination (Split-Path $path -Leaf))
+    : $destination
+
+$uniqueFileName = FindUniqueFileName $destinationFileName
+
+Copy-Item -Path $path -Destination $uniqueFileName -Force
