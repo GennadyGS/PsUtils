142405e9cb4569ac076d6d7d5199bf5667fb73a4
Gennadii Saltyshchak <GennadyGS@gmail.com>
2024-03-28 12:27:57 +0200

    Support expandable variables

--- AddPath.ps1
+++ AddPath.ps1
@@ -4,9 +4,24 @@ param (
 )
 
 $varName = "PATH"
+$userRootKey = [Microsoft.Win32.Registry]::CurrentUser
+$userKeyName = "Environment"
+$machineRootKey = [Microsoft.Win32.Registry]::LocalMachine
+$machineKeyName = "SYSTEM\CurrentControlSet\Control\Session Manager\Environment"
+($rootKey, $keyName) = switch ($scope)
+{
+    "User" { ($userRootKey, $userKeyName) }
+    "Machine" { ($machineRootKey, $machineKeyName) }
+}
+
+$key = $rootKey.OpenSubKey($keyName, $true);
+$options = [Microsoft.Win32.RegistryValueOptions]::DoNotExpandEnvironmentNames;
+$oldValue = $key.GetValue($varName, "", $options);
+
 $fullPath = [System.IO.Path]::GetFullPath($path)
-$oldValue = [Environment]::GetEnvironmentVariable($varName, $scope)
 $newValue = "$oldValue;$fullPath"
+
 "Adding path '$fullPath' to environment variable $varName (scope $scope)..."
-[Environment]::SetEnvironmentVariable($varName, $newValue, $scope)
+$valueKind = [Microsoft.Win32.RegistryValueKind]::ExpandString;
+$key.SetValue($varName, $newValue, $valueKind);
 "Path is successfully added to environment variable"
