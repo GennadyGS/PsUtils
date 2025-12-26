b36e88c96fecc4f307c4dbc8d6b6a2abf15ebecf
Gennadii Saltyshchak <GennadyGS@gmail.com>
2025-12-26 14:31:49 +0200

    Add powershell proxy script

---
+++ PwshShellProxy.cmd
@@ -0,0 +1,30 @@
+@echo off
+setlocal EnableExtensions EnableDelayedExpansion
+
+if "%~1"=="" (
+  echo Usage: %~n0 ^<script.ps1^> [args...]
+  exit /b 64
+)
+
+set "SCRIPT=%~f1"
+shift
+
+set "ARGS="
+
+:loop
+if "%~1"=="" goto run
+
+set "ARG=%~1"
+set "ARG=!ARG:"=""!"
+
+if "%1" NEQ "%~1" set "ARG="""!ARG!""""
+
+set "ARGS=!ARGS! !ARG!"
+
+shift
+goto loop
+
+:run
+pwsh -NoLogo -NoProfile -ExecutionPolicy Bypass -Command "%SCRIPT%" %ARGS%
+
+exit /b %errorlevel%
